dfa787a274e6c01fab2cdf7f613781f9f0ce8774
Jocelyn <791245613@qq.com>
2019-07-31 19:15:13 +0800

    分类页面的数据本地存储完成

--- pages/category/index.js
+++ pages/category/index.js
@@ -13,12 +13,33 @@ Page({
     // 当前的索引 默认为0
     currentIndex: 0,
     // 滚动条距离顶部的高度
-    scrollTop:0
+    scrollTop: 0
   },
   // 用于存放返回的结果
   Cates: [],
   onLoad() {
-    this.getCateInfo()
+    // this.getCateInfo()
+    // 判断一下本地内存 有没有数据
+    // 没有再发送请求
+    let cates = wx.getStorageSync("cates")
+    // console.log(typeof cates);  若本地中
+    if (!cates) { // 不存在的情况
+      this.getCateInfo()  // 发请求 拿数据
+    } else { // 如果数据存在
+      if (Date.now() - cates.time > 1000 * 20) { // 判断一下是否过期
+        // 过期的情况
+        this.getCateInfo() // 重新发请求
+      } else {
+        // 没有过期 把数据拿出来渲染即可
+        this.Cates = cates.data;
+        let leftMenuList = this.Cates.map((v, i) => ({ cat_name: v.cat_name, cat_id: v.cat_id }));
+        let rightGoodsList = this.Cates[0].children;
+        this.setData({
+          leftMenuList,
+          rightGoodsList
+        })
+      }
+    }
   },
   getCateInfo() {
     request({
@@ -26,6 +47,8 @@ Page({
     })
       .then(res => {
         this.Cates = res;
+        // 请求回来的时候 把数据存在本地 同时存入时间 用于判断时间是否过期
+        wx.setStorageSync("cates", { time: Date.now(), data: this.Cates });
         // leftMenuList 等于总的数组中循环出cat_name和cat_id
         let leftMenuList = res.map(v => {
           return { cat_name: v.cat_name, cat_id: v.cat_id }
@@ -52,7 +75,7 @@ Page({
     })
     // 修改数据要用this.setData
     this.setData({
-      scrollTop:0
+      scrollTop: 0
     })
   }
 })
